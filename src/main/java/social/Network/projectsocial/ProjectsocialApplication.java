f5a81bac4a4f4c8c330dc02bf44b08bfac79de84
longkuroko <longsteve.jobs@gmail.com>
2021-08-11 09:07:37 +0700

    first commit

---
+++ src/main/java/social/Network/projectsocial/ProjectsocialApplication.java
@@ -0,0 +1,13 @@
+package social.Network.projectsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/social/Network/projectsocial/ProjectsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package social.Network.projectsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
